f24ae8c7490c93ceb0d086d04fb6eab597e41911
Hayawaseem <hayamwaseem2006@gmail.com>
2024-10-19 19:28:21 +0400

    Coding is Cool
    # Coding is Cool
    """Fill in the blanks in the Python code below to output the phrases "coding is Cool" to the console using variables
    and string concatenation.
    Fill in the blanks below
    word1 =
    word2 =
    word3 =
    # Use string concatenation to combine the variables and print the pharse
    _____(_________) """
    word1 = "Coding"
    word2 = "is"
    word3 = "Cool"
    print (" Coding " + " is " + " Cool ")

--- Exercise 01/Coding is cool.py
+++ Exercise 01/Coding is cool.py
@@ -1,5 +1,13 @@
-#Use string concatenation to combine the variables and print the phrase
-word1 = "Coding" 
+# Coding is Cool 
+"""Fill in the blanks in the Python code below to output the phrases "coding is Cool" to the console using variables 
+and string concatenation.
+Fill in the blanks below 
+word1 =
+word2 =
+word3 =
+# Use string concatenation to combine the variables and print the pharse 
+_____(_________) """
+word1 = "Coding"
 word2 = "is"
 word3 = "Cool"
-print (" Coding " + " is " + " Cool ")
+print (" Coding" + " is " + " Cool ")
---
+++ Exercise 02/Simple Sum.py
@@ -0,0 +1,5 @@
+#Simple Sum 
+A = 8
+B = 10 
+addition = 8 + 10
+print (addition)
