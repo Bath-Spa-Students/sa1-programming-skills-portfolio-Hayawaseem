c97e356b2673c8fc0e1073aa3c9e6cf39da39d97
Hayawaseem <hayamwaseem2006@gmail.com>
2024-11-07 11:50:44 +0400

    Standard.py
    """In this exercise, youll create a simple program that asks the user a question,
    evaluates their answer, and provides feedback.
    Steps:
    1. Write a program that asks the user “What is the capital of France?” and
    waits for their response.
    2. If the uses answer is correct (i.e., “Paris”), the program should print a
    message saying the answer is correct.
    3. If the answer is incorrect, the program should print a message saying the
    answer is wrong."""
    
    # User input
    Capital = input("What is the capital of France?")
      # provides feedback
    if Capital. strip().lower() == "paris" :
        print ("answer is correct!")
    else:
        print ("answer is wrong!")

--- Exercise 04/Standard.py
+++ Exercise 04/Standard.py
@@ -1,17 +1,17 @@
-<<<<<<< HEAD
+"""In this exercise, youll create a simple program that asks the user a question,
+evaluates their answer, and provides feedback.
+Steps:
+1. Write a program that asks the user “What is the capital of France?” and
+waits for their response.
+2. If the uses answer is correct (i.e., “Paris”), the program should print a
+message saying the answer is correct.
+3. If the answer is incorrect, the program should print a message saying the
+answer is wrong."""
+
 # User input 
 Capital = input("What is the capital of France?")
   # provides feedback
-if Capital.strip().lower() == "paris" :
+if Capital. strip().lower() == "paris" :
     print ("answer is correct!")
 else: 
     print ("answer is wrong!") 
-=======
-# User input 
-Capital = input("What is the capital of France?")
-  # provides feedback
-if Capital.strip().lower() == "paris" :
-    print ("answer is correct!")
-else: 
-    print ("answer is wrong!") 
->>>>>>> fa46a74fece6ae86c57430c09f7ac7d0111e8f7b
--- Exercise 08/Simple Search.py
+++ Exercise 08/Simple Search.py
@@ -1,3 +1,10 @@
+"""Write a program that searches for a specific string whitin a list of strings. 
+The list is initialized with specific name (“Jake” “Zac”, “Ian”, “Ron”, “Sam”, “Dave”).
+, and your task is to search for “Sam”.
+Optional Requirements:
+1. Allow the user to input the search term instead of using a predefined value.
+2. Implement the search functionality based on user input."""
+
 # Names 
 Names = ["Jack", "Zac", "Iran", "Ron", "Sam", "Dave"]
 # Search name
