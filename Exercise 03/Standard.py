1a8a5514dd4b6914ff3d35293e93d4189b10456b
Hayawaseem <hayamwaseem2006@gmail.com>
2024-11-07 12:42:58 +0400

    Biography Standard.py
    """In this exercise, youll create a program that stores and prints your name, home-
    town, and age to the console using a Python dictionary.
    
    Steps:
    1. Store the information (name, hometown, and age) as key-value pairs in a
    dictionary.
    2. Print the values on separate lines using a single print() statement.
    3. Use variables with appropriate data types for each piece of information."""
    
    # Biography
    # Information stored
    Name = "Haya"
    Hometown = "Sharjah"
    Age = "18"
    # Personal data stored in dictionary
    dictionary = {"Name : Haya", "Hometown : Sharjah", "Age : 18"}
    # print statement
    print (f"Name : {Name}")
    print (f"Hometown : {Hometown}")
    print (f"Age : {Age}")

---
+++ Exercise 03/Standard.py
@@ -0,0 +1,20 @@
+"""In this exercise, youll create a program that stores and prints your name, home-
+town, and age to the console using a Python dictionary.
+
+Steps:
+1. Store the information (name, hometown, and age) as key-value pairs in a
+dictionary.
+2. Print the values on separate lines using a single print() statement.
+3. Use variables with appropriate data types for each piece of information."""
+
+# Biography 
+# Information stored
+Name = "Haya" 
+Hometown = "Sharjah" 
+Age = "18"
+# Personal data stored in dictionary
+dictionary = {"Name : Haya", "Hometown : Sharjah", "Age : 18"}
+# print statement 
+print (f"Name : {Name}")
+print (f"Hometown : {Hometown}")
+print (f"Age : {Age}") 
