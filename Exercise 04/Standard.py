fa46a74fece6ae86c57430c09f7ac7d0111e8f7b
Hayawaseem <hayamwaseem2006@gmail.com>
2024-11-07 11:17:58 +0400

    Standard.py

---
+++ Exercise 04/Standard.py
@@ -0,0 +1,7 @@
+# User input 
+Capital = input("What is the capital of France?")
+  # provides feedback
+if Capital.strip().lower() == "paris" :
+    print ("answer is correct!")
+else: 
+    print ("answer is wrong!") 
