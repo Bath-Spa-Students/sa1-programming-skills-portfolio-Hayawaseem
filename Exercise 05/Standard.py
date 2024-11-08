1595d8d8055b56dbe65bda86125e32f5ec7a1665
Hayawaseem <hayamwaseem2006@gmail.com>
2024-11-08 07:09:51 +0400

    Standard Day of the month
    """Write a program that tells a user how many days there are in a specific month.
    Use a dictionary to map the month numbers (1-12) to the number of days in
    each month.
    Instructions:
    
    1. Create a Dictionary: Define a dictionary where the keys are month num-
    bers and the values are the number of days in those months.
    
    2. Input Handling: Ask the user to input the month number.
    3. Check and Output: Use an if-else statement to check if the input is valid
    and print the number of days in the corresponding month."""
    
    # Days of the month
    month_days = {
        1: 31,  # January
        2: 28,  # February
        3: 31,  # March
        4: 30,  # April
        5: 31,  # May
        6: 30,  # June
        7: 31,  # July
        8: 31,  # August
        9: 30,  # September
        10: 31, # October
        11: 30, # November
        12: 31,  # December
    }
    # user input
    month = int(input("Enter month number (1-12): "))
    # if-else statement
    if 1 <= month <= 12:
        print (f"Number of days : {month_days[month]}")
    else:
        print (f"Invalid month number")

---
+++ Exercise 05/Standard.py
@@ -0,0 +1,34 @@
+"""Write a program that tells a user how many days there are in a specific month.
+Use a dictionary to map the month numbers (1-12) to the number of days in
+each month.
+Instructions:
+
+1. Create a Dictionary: Define a dictionary where the keys are month num-
+bers and the values are the number of days in those months.
+
+2. Input Handling: Ask the user to input the month number.
+3. Check and Output: Use an if-else statement to check if the input is valid
+and print the number of days in the corresponding month."""
+
+# Days of month
+month_days = {
+    1: 31,  # January
+    2: 28,  # February 
+    3: 31,  # March
+    4: 30,  # April
+    5: 31,  # May
+    6: 30,  # June
+    7: 31,  # July
+    8: 31,  # August
+    9: 30,  # September
+    10: 31, # October
+    11: 30, # November
+    12: 31,  # December
+}
+# user input
+month = int(input("Enter month number (1-12): "))
+# if-else statement
+if 1 <= month <= 12:
+    print (f"Number of days : {month_days[month]}")
+else:
+    print (f"Invalid month number")
---
+++ Exercise 09/Hello.py
@@ -0,0 +1,19 @@
+"""Fill in the blanks in the code below so that the function hello() prints “Hello”
+to the console.
+
+def hello():
+____ # Fill in this blank to print "Hello" to the console
+def main():
+____ # Fill in this blank to call the hello() function
+if __name__ == "__main__":
+main()"""
+
+#Hello function 
+def hello():
+    print ("Hello")
+#Main function
+def main():
+    hello()
+# this line make sures that main() run only if the text is executed.
+if __name__ == "__main__":
+    main()
