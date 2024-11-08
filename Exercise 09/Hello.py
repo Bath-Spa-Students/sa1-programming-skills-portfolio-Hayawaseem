669d00781cfdda0155733d9aa5a21e1fb00b865f
Hayawaseem <hayamwaseem2006@gmail.com>
2024-11-08 07:19:07 +0400

    Hello.py

--- Exercise 09/Hello.py
+++ Exercise 09/Hello.py
@@ -6,7 +6,7 @@ ____ # Fill in this blank to print "Hello" to the console
 def main():
 ____ # Fill in this blank to call the hello() function
 if __name__ == "__main__":
-main()"""
+main()""" 
 
 #Hello function 
 def hello():
